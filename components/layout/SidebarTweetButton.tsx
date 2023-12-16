251b08065e052705d32a72d271cf6c5b7c467413
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-12-16 16:40:05 +0530

    prisma start

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,13 +1,16 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import useLoginModel from '@/hooks/useLoginModel'
+import React, { useCallback } from 'react'
 import { FaFeather } from 'react-icons/fa'
 
 const SidebarTweetButton = () => {
-    const router = useRouter()
+    const loginmodel = useLoginModel()
+    const onClick = useCallback(() => {
+        loginmodel.onOpen()
+    }, [loginmodel])
     return (
-        <div onClick={() => router.push('/')}>
+        <div onClick={onClick}>
             <div className='mt-6 rounded-full h-14 w-14 flex items-center bg-sky-300 hover:bg-opacity-80 transition cursor-pointer lg:hidden'>
-                <FaFeather size={24} color="white" className='mx-4'/>
+                <FaFeather size={24} color="white" className='mx-4' />
             </div>
             <div className='mt-6 hidden lg:block px-4 py-3 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition'>
                 <p className='hidden lg:block text-center font-semibold text-[20px] text-white'>Post</p>
